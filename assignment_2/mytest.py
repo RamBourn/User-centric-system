4f1e99953ce20e89313801e616c0468f6e75f2da
Ram Bourn <35801144+RamBourn@users.noreply.github.com>
2021-08-26 18:59:13 +0800

    Add assignment 2

---
+++ assignment_2/mytest.py
@@ -0,0 +1,222 @@
+from assignment_12 import *
+
+
+#********************************* Tests for all operators*************************************************
+
+def test_Scan():
+    F=Scan("tiny_data_friends.txt")
+    assert F.get_next()==[ATuple([0,1]),ATuple([0,2]),ATuple([1,0]),ATuple([1,2]),ATuple([2,0]),ATuple([2,1])]
+
+def test_Project():
+    R=Scan("tiny_data_ratings.txt")
+    projected=Project(R,[1,2])
+    assert projected.get_next()==[ATuple([0,3]),ATuple([1,5]),ATuple([2,1]),ATuple([0,4]),ATuple([1,1])\
+    ,ATuple([2,2]),ATuple([0,5]),ATuple([1,0]),ATuple([2,3])]
+
+def test_Select():
+    F=Scan("tiny_data_friends.txt")
+    def func(tuple):
+        if tuple.tuple[0]==1:
+            return True
+        else:
+            return False
+    selected=Select(F,func)
+    assert selected.get_next()==[ATuple([1,0]),ATuple([1,2])]
+
+def test_Join():
+    F=Scan("tiny_data_friends.txt")
+    R=Scan("tiny_data_ratings.txt")
+    joined=Join(F,R,1,0)
+    assert joined.get_next()==[ATuple([0,1,1,0,4]),ATuple([0,1,1,1,1]),ATuple([0,1,1,2,2]),ATuple([0,2,2,0,5])\
+        ,ATuple([0,2,2,1,0]),ATuple([0,2,2,2,3]),ATuple([1,0,0,0,3]),ATuple([1,0,0,1,5]),ATuple([1,0,0,2,1])\
+            ,ATuple([1,2,2,0,5]),ATuple([1,2,2,1,0]),ATuple([1,2,2,2,3]),ATuple([2,0,0,0,3]),ATuple([2,0,0,1,5])\
+                ,ATuple([2,0,0,2,1]),ATuple([2,1,1,0,4]),ATuple([2,1,1,1,1]),ATuple([2,1,1,2,2])]
+
+def test_OrderBy():
+    R=Scan("tiny_data_ratings.txt")
+    def cmp(tuple):
+        return tuple.tuple[2]
+    ordered=OrderBy(R,cmp,ASC=False)
+    assert ordered.get_next()==[ATuple([0,1,5]),ATuple([2,0,5]),ATuple([1,0,4]),ATuple([0,0,3]),ATuple([2,2,3])\
+        ,ATuple([1,2,2]),ATuple([0,2,1]),ATuple([1,1,1]),ATuple([2,1,0])]
+
+def test_GroupBy():
+    R=Scan("tiny_data_ratings.txt")
+    def AVG(arr):
+        result=sum(arr)/len(arr)
+        result=round(result,2)
+        return result
+    grouped=GroupBy(R,1,2,AVG)
+    assert grouped.get_next()==[ATuple([0,4.0]),ATuple([1,2.0]),ATuple([2,2.0])]
+
+def test_TopK():
+    F=Scan("tiny_data_friends.txt")
+    top3=TopK(F,3)
+    assert top3.get_next()==[ATuple([0,1]),ATuple([0,2]),ATuple([1,0])]
+
+
+#******************************* Tests for 3 tasks of Assignment 1***********************************************
+
+def test_task1():
+    def AVG(arr):
+        result=sum(arr)/len(arr)
+        result=round(result,2)
+        return result
+    def funcF(tuple):
+        if int(tuple.tuple[0])==0:
+            return True
+        else :
+            return False
+    def funcR(tuple):
+        if int(tuple.tuple[1])==1:
+            return True
+        else :
+            return False
+    F=Scan("tiny_data_friends.txt")
+    R=Scan("tiny_data_ratings.txt")
+    selectedF=Select(F,funcF)
+    selectedR=Select(R,funcR)
+    joined=Join(selectedF,selectedR,1,0)
+    grouped=GroupBy(joined,None,4,AVG)
+    assert grouped.get_next()==[ATuple([0.5])]
+
+def test_task2():
+    F=Scan("tiny_data_friends.txt")
+    R=Scan("tiny_data_ratings.txt")
+    def AVG(arr):
+        result=sum(arr)/len(arr)
+        result=round(result,2)
+        return result
+    def funcF(tuple):
+        if int(tuple.tuple[0])==1:
+            return True
+        else :
+            return False
+    def cmp(tuple):
+        return tuple.tuple[1]
+    selectedF=Select(F,funcF)
+    joined=Join(selectedF,R,1,0)
+    projected=Project(joined,[3,4])
+    grouped=GroupBy(projected,0,1,AVG)
+    ordered=OrderBy(grouped,comparator=cmp,ASC=False)
+    limited=TopK(ordered,1)
+    result=Project(limited,[0])
+    assert result.get_next()==[ATuple([0])]
+
+
+def test_task3():
+    F=Scan("tiny_data_friends.txt")
+    R=Scan("tiny_data_ratings.txt")
+    def funcF(tuple):
+        if int(tuple.tuple[0])==0:
+            return True
+        else :
+            return False
+    def funcR(tuple):
+        if int(tuple.tuple[1])==1:
+            return True
+        else :
+            return False
+    selectedF=Select(F,funcF)
+    selectedR=Select(R,funcR)
+    joined=Join(selectedF,selectedR,1,0)
+    projected=Project(joined,[2,4])
+    histogtamed=Histogram(projected,1)
+    assert histogtamed.get_next()=={0:1,1:1}
+
+
+#********************************Tests for 4 tasks of Assignment 2*************************************************** 
+
+def test_task5():  
+    def funcF(tuple):
+        if int(tuple.tuple[0])==0:
+            return True
+        else :
+            return False
+    def AVG(arr):
+        result=sum(arr)/len(arr)
+        result=round(result,2)
+        return result
+    def cmp(tuple):
+        return tuple.tuple[1]
+    F=Scan("tiny_data_friends.txt",track_prov=True)         
+    R=Scan("tiny_data_ratings.txt",track_prov=True)        
+    selectedF=Select(F,funcF,track_prov=True)
+    joined=Join(selectedF,R,1,0,track_prov=True)
+    projected=Project(joined,[3,4],track_prov=True)
+    grouped=GroupBy(projected,0,1,AVG,track_prov=True)
+    ordered=OrderBy(grouped,comparator=cmp,ASC=False,track_prov=True)
+    limited=TopK(ordered,1,track_prov=True)
+    result=Project(limited,[0],track_prov=True)
+    assert result.get_next()[0].lineage()==[ATuple([0,1]),ATuple([0,2]),ATuple([1,0,4]),ATuple([2,0,5])]
+
+def test_task6():
+    def funcF(tuple):
+        if int(tuple.tuple[0])==0:
+            return True
+        else :
+            return False
+    def funcR(tuple):
+        if int(tuple.tuple[1])==1:
+            return True
+        else :
+            return False
+    def AVG(arr):
+        result=sum(arr)/len(arr)
+        result=round(result,2)
+        return result
+    F=Scan("tiny_data_friends.txt",track_prov=True)        
+    R=Scan("tiny_data_ratings.txt",track_prov=True)     
+    selectedF=Select(F,funcF,track_prov=True)
+    selectedR=Select(R,funcR,track_prov=True)
+    joined=Join(selectedF,selectedR,1,0,track_prov=True)
+    grouped=GroupBy(joined,None,4,AVG,track_prov=True)
+    assert grouped.get_next()[0].where(0)==[('tiny_data_ratings.txt',5,(1,1,1),1)\
+                                            ,('tiny_data_ratings.txt',8,(2,1,0),0)]
+
+def test_task7():
+    F=Scan("tiny_data_friends.txt",propagate_prov=True)        
+    R=Scan("tiny_data_ratings.txt",propagate_prov=True)   
+    def funcF(tuple):
+        if int(tuple.tuple[0])==0:
+            return True
+        else :
+            return False    
+    def AVG(arr):
+        result=sum(arr)/len(arr)
+        result=round(result,2)
+        return result
+    def cmp(tuple):
+        return tuple.tuple[1]
+    selectedF=Select(F,funcF,propagate_prov=True)
+    joined=Join(selectedF,R,1,0,propagate_prov=True)
+    projected=Project(joined,[3,4],propagate_prov=True)
+    grouped=GroupBy(projected,0,1,AVG,propagate_prov=True)
+    ordered=OrderBy(grouped,comparator=cmp,ASC=False,propagate_prov=True)
+    limited=TopK(ordered,1,propagate_prov=True)
+    result=Project(limited,[0],propagate_prov=True)
+    assert result.get_next()[0].how()=="AVG('(f1*r4@4)', '(f2*r7@5)')"       
+
+
+def test_task8():
+    def funcF(tuple):
+        if int(tuple.tuple[0])==0:
+            return True
+        else :
+            return False    
+    def AVG(arr):
+        result=sum(arr)/len(arr)
+        result=round(result,2)
+        return result
+    def cmp(tuple):
+        return tuple.tuple[1]
+    F=Scan("tiny_data_friends.txt",track_prov=True,propagate_prov=True)     
+    R=Scan("tiny_data_ratings.txt",track_prov=True,propagate_prov=True)        
+    selectedF=Select(F,funcF,track_prov=True,propagate_prov=True)
+    joined=Join(selectedF,R,1,0,track_prov=True,propagate_prov=True)
+    projected=Project(joined,[3,4],track_prov=True,propagate_prov=True)
+    grouped=GroupBy(projected,0,1,AVG,track_prov=True,propagate_prov=True)
+    ordered=OrderBy(grouped,comparator=cmp,ASC=False,track_prov=True,propagate_prov=True)
+    limited=TopK(ordered,1,track_prov=True,propagate_prov=True)
+    result=Project(limited,[0],track_prov=True,propagate_prov=True)
+    assert result.get_next()[0].responsible_inputs()==[((0, 1), 1), ((1, 0, 4), 1), ((0, 2), 1), ((2, 0, 5), 1)]
